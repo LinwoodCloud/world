8481b4ae8cd095d33b4034bd367ce48112f8569b
CodeDoctorDE <wusel905@gmail.com>
2021-03-18 18:06:15 +0100

    changing main menu

---
+++ ui/MainScene.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public class MainScene : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    private void Open(string uri)
+    {
+        OS.ShellOpen(uri);
+    }
+
+    public void OnNewsButtonPressed()
+    {
+        Open("https://linwood.tk/blog");
+    }
+    public void OnSourceButtonPressed()
+    {
+        Open("https://github.com/LinwoodCloud/world.git");
+    }
+    public void OnWebsiteButtonPressed()
+    {
+        Open("https://linwood.tk/docs/world/overview");
+    }
+    public void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
