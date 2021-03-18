4779ee27557d6e2fbaf1fd9526d81b7920a62e4f
CodeDoctorDE <proficodergames@gmail.com>
2021-03-18 08:28:18 +0100

    remove errors

--- TerrainSystem/Block.cs
+++ TerrainSystem/Block.cs
@@ -3,27 +3,27 @@ using System;
 
 public abstract class Block
 {
-    public enum Face {
-        TOP, BOTTOM, EAST, WEST, NORTH, SOUTH
-    }
-    public string name
-    {
-        get;
-    }
-    public bool Transparent
-    {
-        get;
-    }
-    public bool Solid
-    {
-        get;
-    }
+	public enum Face {
+		TOP, BOTTOM, EAST, WEST, NORTH, SOUTH
+	}
+	public string name
+	{
+		get;
+	}
+	public bool Transparent
+	{
+		get;
+	}
+	public bool Solid
+	{
+		get;
+	}
 
-    public abstract void BuildVoxelFace(Chunk chunk);
+	//public abstract void BuildVoxelFace(Chunk chunk);
 
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
