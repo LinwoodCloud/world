810a951c4d2fbe76eecefe0955ceeb9448c878a9
CodeDoctorDE <wusel905@gmail.com>
2021-04-04 12:39:24 +0200

    add texture atlas

---
+++ mods/main/blocks/GrassBlock.cs
@@ -0,0 +1,10 @@
+using Godot;
+using LinwoodWorld.System;
+using System;
+
+namespace LinwoodWorld.Main
+{
+    public class GrassBlock : CubitBlock
+    {
+    }
+}
--- system/VoxelWorld.cs
+++ system/VoxelWorld.cs
@@ -4,33 +4,58 @@ using Godot.Collections;
 
 namespace LinwoodWorld.System
 {
-    public class VoxelWorld : Spatial
+    public class VoxelWorld : TextureRect
     {
 
-        private ImageTexture BuildTileSet(Array<Image> images, out Array<Vector2> coords)
+        private ImageTexture BuildTileSet(Array<Texture> textures, out Array<Vector2> coords)
         {
             var tex = new ImageTexture();
             var img = new Image();
-            img.Lock();
-            var size = images.Count * 64;
-            var sizeX = 512 % size;
-            var sizeY = 512 / size * 64;
-            img.Create(sizeX, sizeY, false, Image.Format.Rgb8);
+            var size = textures.Count * 64;
+            var sizeX = 512;
+            var sizeY = Math.Max(64, size / 512 * 64);
+            GD.Print(sizeX, ", ", sizeY);
+            img.Create(sizeX, sizeY, false, Image.Format.Rgba8);
 
             coords = new Array<Vector2>();
-            for (int i = 0; i < images.Count; i++)
+            for (int i = 0; i < textures.Count; i++)
             {
-                var image = images[i];
-                var imageSize = i*64;
-                var x = 512 % imageSize;
-                var y = 512 / imageSize * 64;
+                var texture = textures[i];
+                var image = texture.GetData();
+                image.Convert(Image.Format.Rgba8);
+                var x = i == 0 ? 0 : i * image.GetWidth() % sizeX;
+                var y = i == 0 ? 0 : i * image.GetWidth() / sizeX * 64;
                 var coord = new Vector2(x, y);
+                GD.Print(coord, ", ", image.GetUsedRect());
                 img.BlitRect(image, image.GetUsedRect(), coord);
                 coords.Add(coord);
             }
+            img.SavePng("res://textures/tileset.png");
             tex.CreateFromImage(img);
-            img.SavePng("res://TerrainSystem/tileset.png");
             return tex;
+        }
+        public override void _Ready()
+        {
+            Array<Vector2> coords;
+            var textures = new Array<Texture>();
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/dirt.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/grass_block_side.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/grass_block_top.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/stone.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/dirt.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/grass_block_side.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/dirt.png"));
+            textures.Add(GD.Load<StreamTexture>("res://mods/main/blocks/textures/grass_block_side.png"));
+            Texture = BuildTileSet(textures, out coords);
         }
 
         internal void SetWorldVoxel(object v)
